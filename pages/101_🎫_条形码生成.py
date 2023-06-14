cbe0073d8f7143ed294c74bb1b7d5c76dfd742c4
OneDeepBlue <42636926+OneDeepBlue@users.noreply.github.com>
2023-06-14 15:50:58 +0800

    Update 101_🎫_条形码生成.py

--- pages/101_🎫_条形码生成.py
+++ pages/101_🎫_条形码生成.py
@@ -1,47 +1,33 @@
+import qrcode
 import streamlit as st
-import barcode
-from barcode.writer import ImageWriter
 
-st.set_page_config(page_title="条形码生成", page_icon=":100:", layout="centered",
+st.set_page_config(page_title="二维码生成", page_icon="📷", layout="centered",
                    initial_sidebar_state="expanded")
 
-BARCODE_MAP = ["code128", "ean8", "ean8-guard", "ean13", "ean13-guard", "ean", "gtin", "ean14", "jan", "upc", "upca",
-               "isbn", "isbn13",
-               "gs1", "isbn10", "issn", "code39", "pzn", "itf", "gs1_128", "codabar", "nw-7"]
-
-barcode_map = st.selectbox("条码类型", BARCODE_MAP)
-
 # 获取用户输入的条形码数据
-barcode_str = st.text_area(label="🎫请输入条形码数据", height=200)
+qrcode_str = st.text_area(label="🎫请输入条形码数据", height=200)
 st.caption("支持批量操作，一行一个，理论上无限制条数 :sunglasses:")
-result = barcode_str.split("\n")  # 切割
-barcode_list = [x for x in result if x]  # 去除空元素
-expander = st.expander("更多设置")
-background = expander.color_picker('背景颜色', '#f0f2f6')
-foreground = expander.color_picker('条码颜色', '#000000')
-write = [True, False]
-write_text = expander.radio('显示文本', write, index=0, horizontal=True)
-module_height = expander.slider('条码高度', 1, 30, value=8)
-font_size = expander.slider('字号', 1, 20, value=6)
-text_distance = expander.slider('条码字体间距', 1, 7, value=3)
+result = qrcode_str.split("\n")  # 切割
+qrcode_list = [x for x in result if x]  # 去除空元素
+# 生成二维码对象
+# qr = qrcode.QRCode(version=1, error_correction=qrcode.constants.ERROR_CORRECT_L, box_size=10, border=4)
+# # 添加数据到二维码对象中
+# qr.add_data(data)
+# # 建立二维码矩阵
+# qr.make(fit=True)
+# # 生成二维码图片
+# img = qr.make_image(fill_color="black", back_color="white")
 
-if barcode_list:
-    st.write("条码数量：", len(barcode_list))
+if qrcode_list:
+    st.write("条码数量：", len(qrcode_list))
     col1, col2 = st.columns(2)
-    b_map = barcode.get_barcode_class(barcode_map)
-    for i in range(len(barcode_list)):
-        bar_code = b_map(barcode_list[i], writer=ImageWriter())
-        bar_code.default_writer_options['module_height'] = module_height
-        bar_code.default_writer_options['font_size'] = font_size
-        bar_code.default_writer_options['text_distance'] = text_distance
-        bar_code.default_writer_options['write_text'] = write_text
-        bar_code.default_writer_options['background'] = background
-        bar_code.default_writer_options['foreground'] = foreground
-        # bar_code.default_writer_options['background'] = 'white'
-        # bar_code.default_writer_options['module_width'] = module_width
+    for i in range(len(qrcode_list)):
+        img = qrcode.make(qrcode_list[i])
         if i % 2:
             with col2:
-                st.image(bar_code.render())
+                st.image(img.get_image())
+                st.caption(qrcode_list[i])
         else:
             with col1:
-                st.image(bar_code.render())
+                st.image(img.get_image())
+                st.caption(qrcode_list[i])

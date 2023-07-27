985db1fb72e337e6400d3fee5a46257d7bc349cf
OneDeepBlue <42636926+OneDeepBlue@users.noreply.github.com>
2023-07-27 21:57:57 +0800

    Update 🏠_Home.py

--- 🏠_Home.py
+++ 🏠_Home.py
@@ -3,8 +3,5 @@
 import streamlit as st
 
 st.write("[条形码生成](/条形码生成)", target="_self")
-st.experimental_set_query_params(
-    show_map=True,
-    selected=["asia", "/条形码生成"],
-)
-st.title("条形码生成", anchor="#条形码生成")
+
+st.title("条形码生成")
